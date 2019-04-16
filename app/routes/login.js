b90ec9d0b84815a37e0b5b484fac7f1cb02aaf03
Wilmer A Rubio Rojas <rubiorojasw@gmail.com>
2019-04-16 07:12:51 -0500

    Correcciones Redis Login, entrega 3 proyecto 1

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,19 +2,58 @@ const express = require('express');
 const router = require('express').Router();
 const loginController = require('../controllers/loginController');
 
+const AWS = require('aws-sdk');
+AWS.config.loadFromPath(__dirname + '/config.json');
+
+const log4js = require('log4js');
+var fs = require('fs');
+
+//Log de pruebas
+log4js.configure({
+    appenders: { cheese: { type: 'file', filename: './sessions.log' } },
+    categories: { default: { appenders: ['cheese'], level: 'error' } }
+});
+
+const logger = log4js.getLogger('cheese');
+
 router.post('/', (req, res) => {
+
     console.log(req.session);
+
     if (!req.session.key) {
+
         loginController.findAdmin(req).then((result) => {
-            console.log(result);
             req.session.key = result;
             res.status(200).json(result);
         }).catch((err) => {
-            console.log(err);
             res.status(400).json(err);
         });
+
     } else {
-        console.log('por aca entro!');
+
+        console.log('REDIS:');
+
+        logger.fatal(`Inicio session: ${JSON.stringify(req.session.key.user)}`);
+
+        fs.readFile('./sessions.log', (err, file) => {
+
+            if (err) console.log('read log');
+
+            const logParam = {
+                Bucket: 'svoice',
+                Key: `logs/redis/${file}`,
+                Body: file,
+            };
+
+            const logS3 = new AWS.S3();
+
+            logS3.upload(logParam, function(err, data) {
+                if (err) console.log('upload log');
+                console.log('file uploaded');
+            });
+
+        });
+
         res.status(200).json(req.session.key);
     }
 });
